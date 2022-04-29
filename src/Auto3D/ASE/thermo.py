fb5994ece63ab73d0da74cb97426ea3b3640ccd1
Zhen (Jack) Liu <liu5@andrew.cmu.edu>
2022-04-28 22:59:34 -0400

    remove submodule & add submodule scripts

---
+++ src/Auto3D/ASE/thermo.py
@@ -0,0 +1,364 @@
+#!/usr/bin/env python
+"""
+Calculating thermodynamic perperties using Auto3D output
+"""
+import sys
+import os
+root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(root)
+import warnings
+import torch
+from ase import Atoms
+from ase.optimize import BFGS
+from openbabel import pybel
+from ase.vibrations import Vibrations
+from ase.thermochemistry import IdealGasThermo
+import ase.calculators.calculator
+try:
+    from ..batch_opt.ANI2xt import ANI2xt
+except:
+    pass
+import torchani
+
+
+ev2hatree = 1/27.211385  #1 Hartree = 27.211385 eV
+class EnForce_ANI(torch.nn.Module):
+    """Takes in an torch model, then defines forward functions for it.
+    Especially useful for AIMNET (class torch.jit)
+    Arguments:
+        name: ['ANI2xt', 'AIMNET']
+        model_parameters: path to the state dictionary
+    Returns:
+        the energies and forces for the input molecules.
+    """
+    def __init__(self, name, model_parameters=None, device=torch.device("cpu")):
+        super().__init__()
+        self.name = name
+        self.model_parameters = model_parameters
+        if self.name == 'ANI2xt':
+            model = ANI2xt(device, model_parameters)
+        elif self.name == "AIMNET":
+            model = torch.jit.load(model_parameters, map_location=device)
+        self.model = model
+        self.device = device
+
+    def forward(self, coord, numbers, charge=0):
+        """Calculate the energies and forces for input molecules. Called by self.forward_batched
+        
+        Arguments:
+            coord: coordinates for all input structures. size (B, N, 3), where
+                  B is the number of structures in coord, N is the number of
+                  atoms in each structure, 3 represents xyz dimensions.
+            numbers: the atomic numbers
+            
+        Returns:
+            energies
+            forces
+        """
+
+        if self.name == "AIMNET":
+            charge = torch.tensor(charge, dtype=torch.float, device=self.device)
+            d = self.model(dict(coord=coord, numbers=numbers, charge=charge))
+            e = (d['energy'] + d['disp_energy']).to(torch.double)
+            g = torch.autograd.grad([e.sum()], [coord])[0]
+            assert g is not None
+            f = -g
+        elif self.name == "ANI2xt":
+            # d = {1:0, 6:1, 7:2, 8:3, 16:4, 9:5, 17:6}
+            d = {1:0, 6:1, 7:2, 8:3, 9:4, 16:5, 17:6}
+            numbers2 = numbers.to('cpu').apply_(d.get).to(self.device)
+            e, f = self.model(numbers2, coord)
+
+        return e, f
+
+class Calculator(ase.calculators.calculator.Calculator):
+    """ASE calculator interface for AIMNET and ANI2xt"""
+    implemented_properties = ['energy', 'forces']
+    def __init__(self, model, charge=0):
+        super().__init__()
+        self.charge = charge
+        self.species = {'H':1, 'C':6, 'N':7, 'O':8, 'F':9, 'Si':14, 'P':15,
+                        'S':16, 'Cl':17, 'As':33, 'Se':34, 'Br':35, 'I':53,
+                        'B':5}
+        self.model = model 
+        for p in self.model.parameters():
+            p.requires_grad_(False)
+        a_parameter = next(self.model.parameters())
+        self.device = a_parameter.device
+        self.dtype = a_parameter.dtype
+
+    def calculate(self, atoms=None, properties=['energy'],
+                  system_changes=ase.calculators.calculator.all_changes):
+        super().calculate(atoms, properties, system_changes)
+
+        species = torch.tensor([self.species[symbol] for symbol in self.atoms.get_chemical_symbols()],
+                               dtype=torch.long, device=self.device)
+        coordinates = torch.tensor(self.atoms.get_positions()).to(self.device).to(self.dtype)
+        coordinates = coordinates.requires_grad_(True)
+
+        species = species.unsqueeze(0)
+        coordinates = coordinates.unsqueeze(0)
+        
+        energy, forces = self.model(coordinates, species, self.charge)
+        self.results['energy'] = energy.item()
+        self.results['forces'] = forces.squeeze(0).to('cpu').numpy()
+
+
+def get_mol_idx_t1(mol):
+    """Get idx and temperature from openbabel molecule
+    example: /Users/liu5/Documents/tautomer/dG/dG3/DFT_output_analyze2/817-2-473.xyz
+    """
+
+    idx = str(mol).split()[1].strip().split("/")[-1].strip().split(".")[0]
+    T = int(idx.split("-")[-1])
+    return (idx, T)
+
+def get_mol_idx_t2(mol):
+    """Get idx and temperature from openbabel molecule"""
+    idx = mol.data['ID']
+    ref_idx_t = idx.split("_")[0].strip()
+    T = round(float(ref_idx_t.split("-")[-1]))
+    return (idx, T)
+
+def get_mol_idx_t3(mol):
+    "Setting default index and temperature"
+    idx = ""
+    T = 298
+    return (idx, T)
+
+def get_mol_idx_t4(mol):
+    idx = mol.title.strip()
+    T = 298
+    return (idx, T)
+
+def get_mol_idx_t5(mol):
+    idx = mol.data["ID"].strip()
+    T = 298
+    return (idx, T)
+
+def get_mol_idx_t6(mol):
+    idx = mol.data["ID"].strip()
+    T = float(mol.data["T"])
+    return (idx, T)
+
+
+def calc_thermo(path: str, model_name: str, get_mol_idx_t:callable, gpu_idx=0, opt_tol=0.005, opt_steps=10000):
+    """ASE interface for calculation thermo properties using ANI2xt and AIMNET
+    path: Input sdf file
+    model_name: ANI2x, ANI2xt or AIMNET
+    get_mol_idx_t: a functioin that returns (idx, T) from a pybel mol object
+    gpu_idx: GPU cuda index
+    opt_tol: Convergence_threshold for geometry optimization
+    opt_steps: Maximum geometry optimizaiton steps"""
+    #Prepare output name
+    dir = os.path.dirname(path)
+    basename = os.path.basename(path).split(".")[0] + f"_{model_name}_G.sdf"
+    outpath = os.path.join(dir, basename)
+
+    #internal parameters
+    out_mols = []
+    mols_failed = []
+    species2numbers = {'H':1, 'C':6, 'N':7, 'O':8, 'F':9, 'Si':14, 'P':15,
+                       'S':16, 'Cl':17, 'As':33, 'Se':34, 'Br':35, 'I':53, 'B':5}
+    numbers2species = dict([(val, key) for (key, val) in species2numbers.items()])
+    if torch.cuda.is_available():
+        device = torch.device(f"cuda:{gpu_idx}")
+    else:
+        device = torch.device("cpu")
+    if model_name == "ANI2xt":
+        dict_path = os.path.join(root, "models/ani2xt_seed0.pt")
+        model = EnForce_ANI('ANI2xt', dict_path, device=device)
+    elif model_name == "AIMNET":
+        dict_path = os.path.join(root, "models/aimnet2nqed_pc14iall_b97m_sae.jpt")
+        model = EnForce_ANI('AIMNET', dict_path, device=device)
+    elif model_name == "ANI2x":
+        calculator = torchani.models.ANI2x().to(device).ase()
+    else:
+        raise ValueError("model has to be 'ANI2x', 'ANI2xt' or 'AIMNET'")
+
+    mols = pybel.readfile("sdf", path)
+    for mol in mols:
+        coord = [a.coords for a in mol.atoms]
+        charge = mol.charge
+        species = [numbers2species[a.atomicnum] for a in mol.atoms]
+        atoms = Atoms(species, coord)
+        
+        if model_name != "ANI2x":
+            calculator = Calculator(model, charge)
+        atoms.set_calculator(calculator)
+
+
+        opt = BFGS(atoms)
+        opt.run(fmax=opt_tol, steps=opt_steps)
+        e = atoms.get_potential_energy()
+
+        idx, T = get_mol_idx_t(mol)
+
+        vib = Vibrations(atoms)
+        try:
+            vib.clean()
+            vib.run()
+            vib_e = vib.get_energies()
+
+            thermo = IdealGasThermo(vib_energies=vib_e,
+                                    potentialenergy=e,
+                                    atoms=atoms,
+                                    geometry='nonlinear',
+                                    symmetrynumber=1, spin=0)
+            H = thermo.get_enthalpy(temperature=T) * ev2hatree
+            S = thermo.get_entropy(temperature=T, pressure=101325) * ev2hatree
+            G = thermo.get_gibbs_energy(temperature=T, pressure=101325) * ev2hatree
+            vib.clean()
+            print(G)
+
+            mol.data['H_hatree'] = H
+            mol.data['S_hatree'] = S
+            mol.data['T_K'] = T
+            mol.data['G_hatree'] = G
+            mol.data['E_hatree'] = e * ev2hatree
+            out_mols.append(mol)
+
+            #Updating ASE atoms coordinates into pybel mol
+            coord = atoms.get_positions()
+            for atom, c in zip(mol.atoms, coord):
+                atom.OBAtom.SetVector(*c)
+        
+        except:
+            print("Failed: ", idx)
+            vib.clean()
+            mols_failed.append(mol)
+
+    print(len(mols_failed))
+    print(len(out_mols))
+    with open(outpath, 'w+') as f:
+        all_mols = out_mols + mols_failed
+        for mol in all_mols:
+            f.write(mol.write('sdf'))
+    return outpath
+
+
+if __name__ == "__main__":
+    import os
+    from openbabel import pybel
+    from ase.vibrations import Vibrations
+    from ase.thermochemistry import IdealGasThermo
+    import argparse
+
+    
+    ev2hatree = 1/27.211385  #1 Hartree = 27.211385 eV
+    parser = argparse.ArgumentParser(description="ASE interface for calculation G for ANI2xt and AIMNET")
+    parser.add_argument("path", type=str, help="Input sdf file")
+    parser.add_argument("model", type=str, help="ANI2x, ANI2xt or AIMNET")
+    parser.add_argument("idx_t", type=int, help="Need to define a function to get index and T from openbabel molecule")
+    parser.add_argument("--gpu_idx", default=0, type=int, help="GPU cuda index")
+    parser.add_argument("--convergence_threshold", default=0.0005, type=float, 
+                        help="Convergence_threshold for geometry optimization")
+    parser.add_argument("--opt_steps", default=10000, type=int,
+                        help="Maximum geometry optimizaiton steps")
+
+    args = parser.parse_args()
+    path = args.path
+    model_name = args.model
+    gpu_idx = args.gpu_idx
+    idx_t = args.idx_t
+    opt_steps = args.opt_steps
+    opt_tol = args.convergence_threshold
+
+    dir = os.path.dirname(path)
+    basename = os.path.basename(path).split(".")[0] + f"_{model_name}_G.sdf"
+    outpath = os.path.join(dir, basename)
+    out_mols = []
+    mols_failed = []
+    species2numbers = {'H':1, 'C':6, 'N':7, 'O':8, 'F':9, 'Si':14, 'P':15,
+                       'S':16, 'Cl':17, 'As':33, 'Se':34, 'Br':35, 'I':53, 'B':5}
+    numbers2species = dict([(val, key) for (key, val) in species2numbers.items()])
+
+    if torch.cuda.is_available():
+        device = torch.device(f"cuda:{gpu_idx}")
+    else:
+        device = torch.device("cpu")
+    if model_name == "ANI2xt":
+        dict_path = os.path.join(root, "models/ani2xt_seed0.pt")
+        model = EnForce_ANI('ANI2xt', dict_path, device=device)
+    elif model_name == "AIMNET":
+        dict_path = os.path.join(root, "models/aimnet2nqed_pc14iall_b97m_sae.jpt")
+        model = EnForce_ANI('AIMNET', dict_path, device=device)
+    elif model_name == "ANI2x":
+        calculator = torchani.models.ANI2x().to(device).ase()
+    else:
+        raise ValueError("model has to be 'ANI2x', 'ANI2xt' or 'AIMNET'")
+
+
+    if int(idx_t) == 1:
+        get_mol_idx_t = get_mol_idx_t1
+    elif int(idx_t) == 2:
+        get_mol_idx_t = get_mol_idx_t2
+    elif int(idx_t) == 3:
+        get_mol_idx_t = get_mol_idx_t3
+    elif int(idx_t) == 4:
+        get_mol_idx_t = get_mol_idx_t4
+    elif int(idx_t) == 5:
+        get_mol_idx_t = get_mol_idx_t5
+    elif int(idx_t) == 6:
+        get_mol_idx_t = get_mol_idx_t6
+    else:
+        raise ValueError("Has to define a function for get index and temperature for OB mol object.")
+
+    mols = pybel.readfile("sdf", path)
+    for mol in mols:
+        coord = [a.coords for a in mol.atoms]
+        charge = mol.charge
+        species = [numbers2species[a.atomicnum] for a in mol.atoms]
+        atoms = Atoms(species, coord)
+        
+        if model_name != "ANI2x":
+            calculator = Calculator(model, charge)
+        atoms.set_calculator(calculator)
+
+
+        opt = BFGS(atoms)
+        opt.run(fmax=opt_tol, steps=opt_steps)
+        e = atoms.get_potential_energy()
+
+        idx, T = get_mol_idx_t(mol)
+
+        vib = Vibrations(atoms)
+        try:
+            vib.clean()
+            vib.run()
+            vib_e = vib.get_energies()
+
+            thermo = IdealGasThermo(vib_energies=vib_e,
+                                    potentialenergy=e,
+                                    atoms=atoms,
+                                    geometry='nonlinear',
+                                    symmetrynumber=1, spin=0)
+            H = thermo.get_enthalpy(temperature=T) * ev2hatree
+            S = thermo.get_entropy(temperature=T, pressure=101325) * ev2hatree
+            G = thermo.get_gibbs_energy(temperature=T, pressure=101325) * ev2hatree
+            vib.clean()
+            print(G)
+
+            mol.data['H_hatree'] = H
+            mol.data['S_hatree'] = S
+            mol.data['T_K'] = T
+            mol.data['G_hatree'] = G
+            mol.data['E_hatree'] = e * ev2hatree
+            out_mols.append(mol)
+
+            #Updating ASE atoms coordinates into pybel mol
+            coord = atoms.get_positions()
+            for atom, c in zip(mol.atoms, coord):
+                atom.OBAtom.SetVector(*c)
+        
+        except:
+            print("Failed: ", idx)
+            vib.clean()
+            mols_failed.append(mol)
+
+    print(len(mols_failed))
+    print(len(out_mols))
+    with open(outpath, 'w+') as f:
+        all_mols = out_mols + mols_failed
+        for mol in all_mols:
+            f.write(mol.write('sdf'))
---
+++ src/Auto3D/isomer_engine.py
@@ -0,0 +1,305 @@
+#!/usr/bin/env python
+"""
+Enumerating stereoisomers for each SMILES representation with RDKit.
+"""
+import warnings
+import os
+import glob
+import collections
+from openbabel import pybel
+from rdkit import Chem
+from rdkit.Chem import AllChem
+from rdkit.Chem.EnumerateStereoisomers import EnumerateStereoisomers
+from rdkit.Chem.EnumerateStereoisomers import StereoEnumerationOptions
+from rdkit.Chem.MolStandardize import rdMolStandardize
+from .utils import hash_enumerated_smi_IDs, combine_smi, amend_configuration_w
+from .utils import remove_enantiomers
+try:
+    from openeye import oechem
+    from openeye import oequacpac
+    from openeye import oeomega
+except:
+    pass
+from tqdm import tqdm
+
+
+class tautomer_engine(object):
+    """Enemerate possible tautomers for the input
+    
+    Arguments:
+        mode: rdkit or oechem
+        input: smi file
+        output: smi file
+        
+    """
+    def __init__(self, mode, input, out):
+        self.mode = mode
+        self.input = input
+        self.output = out
+
+    def oe_taut(self):
+        """OEChem enumerating tautomers, modified from
+        https://docs.eyesopen.com/toolkits/python/quacpactk/examples_summary_getreasonabletautomers.html"""
+        ifs = oechem.oemolistream()
+        ifs.open(self.input)
+
+        ofs = oechem.oemolostream()
+        ofs.open(self.output)
+
+        tautomerOptions = oequacpac.OETautomerOptions()
+        pKaNorm = False
+
+        for mol in ifs.GetOEGraphMols():
+            for tautomer in oequacpac.OEGetReasonableTautomers(mol, tautomerOptions, pKaNorm):
+                oechem.OEWriteMolecule(ofs, tautomer)
+        
+        # Appending input smiles into output
+        combine_smi([self.input, self.output], self.output)
+
+    def rd_taut(self):
+        """RDKit enumerating tautomers"""
+        enumerator = rdMolStandardize.TautomerEnumerator()
+        smiles = []
+        with open(self.input, 'r') as f:
+            data = f.readlines()
+            for line in data:
+                line = line.strip().split()
+                smi, idx = line[0], line[1]
+                smiles.append((smi, idx))
+        tautomers = []
+        for smi_idx in smiles:
+            smi, idx = smi_idx
+            mol = Chem.MolFromSmiles(smi)
+            tauts = enumerator.Enumerate(mol)
+            for taut in tauts:
+                tautomers.append((Chem.MolToSmiles(taut), idx))
+        with open(self.output, 'w+') as f:
+            for smi_idx in tautomers:
+                smi, idx = smi_idx
+                line = smi.strip() + ' ' + str(idx.strip()) + '\n'
+                f.write(line)
+
+    def run(self):
+        if self.mode == 'oechem':
+            self.oe_taut()
+        elif self.mode == 'rdkit':
+            self.rd_taut()
+        else:
+            raise ValueError(f'{self.mode} must be one of "oechem" or "rdkit".')
+
+class rd_isomer(object):
+    """
+    Enumerating stereoisomers for each SMILES representation with RDKit.
+
+    Arguments:
+        smi: A smi file containing SMILES and IDs.
+        smiles_enumerated: A smi containing cis/trans isomers for the smi file.
+        smiles_hashed: For smiles_enumerated, each ID is hashed.
+        enumerated_sdf: for smiles_hashed, generating possible 3D structures.
+        job_name: as the name suggests.
+        max_confs: maximum number of conformers for each smi.
+        threshold: Maximum RMSD to be considered as duplicates.
+    """
+    def __init__(self, smi, smiles_enumerated, smiles_enumerated_reduced,
+                 smiles_hashed, enumerated_sdf, job_name, max_confs, threshold, np,
+                 flipper=True):
+        self.input = smi
+        self.n_conformers = max_confs
+        self.enumerate = {}
+        self.enumerated_smi_path = smiles_enumerated
+        self.enumerated_smi_path_reduced = smiles_enumerated_reduced
+        self.enumerated_smi_hashed_path = smiles_hashed
+        self.enumerated_sdf = enumerated_sdf
+        self.num2sym = {1: 'H', 6: 'C', 8: 'O', 7: 'N',
+                        9: 'F', 16: 'S', 17: 'Cl'}
+        self.rdk_tmp = os.path.join(job_name, 'rdk_tmp')
+        os.mkdir(self.rdk_tmp)
+        self.threshold = threshold
+        self.np = np
+        self.flipper = flipper
+
+    @staticmethod
+    def read(input):
+        outputs = {}
+        with open(input, 'r') as f:
+            data = f.readlines()
+        for line in data:
+            smiles, name = tuple(line.strip().split())
+            outputs[name.strip()] = smiles.strip()
+        return outputs
+
+    @staticmethod
+    def enumerate_func(mol):
+        """Enumerate the R/S and cis/trans isomers
+        
+        Argument:
+            mol: rd mol object
+            
+        Return:
+            isomers: a list of SMILES"""
+        opts = StereoEnumerationOptions(unique=True)
+        isomers = tuple(EnumerateStereoisomers(mol, options=opts))
+        isomers = sorted(Chem.MolToSmiles(x, isomericSmiles=True, doRandom=False) for x in isomers)
+        return isomers
+
+    def write_enumerated_smi(self):
+        with open(self.enumerated_smi_path, 'w+') as f:
+            for name, smi in self.enumerate.items():
+                for i, isomer in enumerate(smi):
+                    new_name = str(name).strip() + '_' + str(i)
+                    line = isomer.strip() + '\t' + new_name + '\n'
+                    f.write(line)
+
+    def conformer_func(self, smi_name):
+        """smi_name is a tuple (smiles, name)"""
+        smi, name = smi_name
+        mol = Chem.AddHs(Chem.MolFromSmiles(smi))
+        AllChem.EmbedMultipleConfs(mol, numConfs=self.n_conformers,
+                                   randomSeed=42, numThreads=self.np,
+                                   pruneRmsThresh=self.threshold)
+        files = []
+        for i in range(mol.GetNumConformers()):
+            basename = name.strip() + f"_{i}.sdf"
+            mol_ID = basename.split(".")[0].strip()
+            mol.SetProp('ID', mol_ID)
+            file_path = os.path.join(self.rdk_tmp, basename)
+            
+            writer = Chem.SDWriter(file_path)
+            writer.write(mol, confId=i)
+            writer.close()
+            files.append(file_path)
+
+    def combine_SDF(self, SDFs, out):
+        """Combine and sort SDF files in folder into a single file"""
+        paths = os.path.join(SDFs, '*.sdf')
+        files = glob.glob(paths)
+        dict0 = {}
+        for file in files:
+            mols = pybel.readfile('sdf', file)
+            for mol in mols:
+                idx = mol.data['ID']
+                mol.title = idx
+                dict0[idx] = mol
+            dict0 = collections.OrderedDict(sorted(dict0.items()))
+
+        f = pybel.Outputfile('sdf', out)
+        for idx, mol in sorted(dict0.items()):
+            f.write(mol)
+        f.close()  
+
+    def run(self):
+        """
+        When called, enumerate 3 dimensional structures for the input file and
+        writes all structures in 'job_name/smiles_enumerated.sdf'
+        """
+        if self.flipper:
+            print("Enumerating cis/tran isomers for unspecified double bonds...")
+            print("Enumerating R/S isomers for unspecified atomic centers...")
+            smiles_og = self.read(self.input)
+            for name, smiles in smiles_og.items():
+                # mol = Chem.AddHs(Chem.MolFromSmiles(smiles))
+                mol = Chem.MolFromSmiles(smiles)
+                isomers = self.enumerate_func(mol)
+                self.enumerate[name] = isomers
+            self.write_enumerated_smi()
+            print("Removing enantiomers...")
+            amend_configuration_w(self.enumerated_smi_path)
+            remove_enantiomers(self.enumerated_smi_path, self.enumerated_smi_path_reduced)
+            hash_enumerated_smi_IDs(self.enumerated_smi_path_reduced,
+                                    self.enumerated_smi_hashed_path)
+        else:
+            hash_enumerated_smi_IDs(self.input,
+                                    self.enumerated_smi_hashed_path)
+
+        print("Enumerating conformers/rotamers, removing duplicates...")
+        smiles2 = self.read(self.enumerated_smi_hashed_path)
+
+        smi_name_tuples = [(smi, name) for name, smi in smiles2.items()]
+        # if __name__ == "__main__":
+        #     with Pool(self.np) as p:
+        #         p.map(self.conformer_func, smi_name_tuples)
+        for smi_name in smi_name_tuples:
+            self.conformer_func(smi_name)
+
+        self.combine_SDF(self.rdk_tmp, self.enumerated_sdf)
+
+
+def oe_flipper(input, out):
+    """helper function for oe_isomer"""
+    ifs = oechem.oemolistream()
+    ifs.open(input)
+    ofs = oechem.oemolostream()
+    ofs.open(out)
+
+    flipperOpts = oeomega.OEFlipperOptions()
+    flipperOpts.SetWarts(True)
+    flipperOpts.SetMaxCenters(12)
+    flipperOpts.SetEnumNitrogen(True)
+    flipperOpts.SetEnumBridgehead(True)
+    flipperOpts.SetEnumEZ(False)
+    flipperOpts.SetEnumRS(False)
+    for mol in ifs.GetOEMols():
+        # oechem.OEThrow.Info("Title: %s" % mol.GetTitle())
+        for enantiomer in oeomega.OEFlipper(mol.GetActive(), flipperOpts):
+            enantiomer = oechem.OEMol(enantiomer)
+            oechem.OEWriteMolecule(ofs, enantiomer)
+
+def oe_isomer(mode, input, smiles_enumerated, smiles_reduced, smiles_hashed, output, max_confs, threshold, flipper=True):
+    """Generating R/S, cis/trans and conformers using omega
+    Arguments:
+        mode: 'classic' or 'macrocycle'
+        input: input smi file
+        output: output SDF file
+        flipper: optional R/S and cis/trans enumeration"""
+    
+    if mode == "classic":
+        omegaOpts = oeomega.OEOmegaOptions()
+    elif mode == "macrocycle":
+        omegaOpts = oeomega.OEMacrocycleOmegaOptions()
+    else:
+        raise ValueError(f"mode has to be 'classic' or 'macrocycle', but received {mode}.")
+    omegaOpts.SetParameterVisibility(oechem.OEParamVisibility_Hidden) 
+    omegaOpts.SetParameterVisibility("-rms", oechem.OEParamVisibility_Simple)
+    omegaOpts.SetParameterVisibility("-ewindow", oechem.OEParamVisibility_Simple)
+    omegaOpts.SetParameterVisibility("-maxconfs", oechem.OEParamVisibility_Simple)
+    
+    omegaOpts.SetMaxConfs(max_confs)
+    omegaOpts.SetEnergyWindow(999)
+    if mode == "classic":
+        omegaOpts.SetFixRMS(threshold)  #macrocycle mode does not have the attribute 'SetFixRMS'
+        omegaOpts.SetStrictStereo(False)
+        omegaOpts.SetWarts(True)
+    else:
+        omegaOpts.SetIterCycleSize(1000)
+        omegaOpts.SetMaxIter(2000)
+    opts = oechem.OESimpleAppOptions(omegaOpts, "Omega", oechem.OEFileStringType_Mol, oechem.OEFileStringType_Mol3D)
+
+    omegaOpts.UpdateValues(opts)
+    if mode == "classic":
+        omega = oeomega.OEOmega(omegaOpts)
+    else:
+        omega = oeomega.OEMacrocycleOmega(omegaOpts)
+
+    if flipper:
+        print("Enumerating stereoisomers.")
+        oe_flipper(input, smiles_enumerated)
+        amend_configuration_w(smiles_enumerated)
+        remove_enantiomers(smiles_enumerated, smiles_reduced)
+        ifs = oechem.oemolistream()
+        ifs.open(smiles_reduced)
+    else:
+        ifs = oechem.oemolistream()
+        ifs.open(input)
+    ofs = oechem.oemolostream()
+    ofs.open(output)
+
+    print("Enumerating conformers.")
+    for mol in tqdm(ifs.GetOEMols()):
+        # oechem.OEThrow.Info("Title: %s" % mol.GetTitle())
+        ret_code = omega.Build(mol)
+        if ret_code == oeomega.OEOmegaReturnCode_Success:
+            oechem.OEWriteMolecule(ofs, mol)
+        else:
+            oechem.OEThrow.Warning("%s: %s" % (mol.GetTitle(), oeomega.OEGetOmegaError(ret_code)))
+
+    return 0
