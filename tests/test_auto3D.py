02ef643e0c1b20af7752c54ad53bfc96427a8bcf
Zhen (Jack) Liu <liu5@andrew.cmu.edu>
2022-06-12 18:33:30 -0400

    more tests for Auto3D

--- tests/test_auto3D.py
+++ tests/test_auto3D.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 from send2trash import send2trash
 from Auto3D.auto3D import options, main
 
@@ -7,9 +8,120 @@ folder = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 path = os.path.join(folder, "tests/files/smiles2.smi")
 
 
-def test_auto3D():
+def test_auto3D_rdkit_aimnet():
     """Check that the program runs"""
-    args = options(path, k=1, use_gpu=False, convergence_threshold=0.01)
+    args = options(path, k=1, use_gpu=False, convergence_threshold=0.1,
+                   isomer_engine="rdkit", optimizing_engine="AIMNET")
     out = main(args)
     out_folder = os.path.dirname(os.path.abspath(out))
-    send2trash(out_folder)
+    try:
+        send2trash(out_folder)
+    except:
+        shutil.rmtree(out_folder)
+
+
+def test_auto3D_rdkit_ani2xt():
+    """Check that the program runs"""
+    args = options(path, k=1, use_gpu=False, convergence_threshold=0.1,
+                   isomer_engine="rdkit", optimizing_engine="ANI2xt")
+    out = main(args)
+    out_folder = os.path.dirname(os.path.abspath(out))
+    try:
+        send2trash(out_folder)
+    except:
+        shutil.rmtree(out_folder)
+
+def test_auto3D_rdkit_ani2x():
+    """Check that the program runs"""
+    args = options(path, k=1, use_gpu=False, convergence_threshold=0.1,
+                   isomer_engine="rdkit", optimizing_engine="ANI2x")
+    out = main(args)
+    out_folder = os.path.dirname(os.path.abspath(out))
+    try:
+        send2trash(out_folder)
+    except:
+        shutil.rmtree(out_folder)
+
+
+def test_auto3D_omega_aimnet():
+    """Check that the program runs"""
+    args = options(path, k=1, use_gpu=False, convergence_threshold=0.1,
+                   isomer_engine="omega", optimizing_engine="AIMNET")
+    out = main(args)
+    out_folder = os.path.dirname(os.path.abspath(out))
+    try:
+        send2trash(out_folder)
+    except:
+        shutil.rmtree(out_folder)
+
+
+def test_auto3D_omega_ani2xt():
+    """Check that the program runs"""
+    args = options(path, k=1, use_gpu=False, convergence_threshold=0.1,
+                   isomer_engine="omega", optimizing_engine="ANI2xt")
+    out = main(args)
+    out_folder = os.path.dirname(os.path.abspath(out))
+    try:
+        send2trash(out_folder)
+    except:
+        shutil.rmtree(out_folder)
+
+
+def test_auto3D_omega_ani2x():
+    """Check that the program runs"""
+    args = options(path, k=1, use_gpu=False, convergence_threshold=0.1,
+                   isomer_engine="omega", optimizing_engine="ANI2x")
+    out = main(args)
+    out_folder = os.path.dirname(os.path.abspath(out))
+    try:
+        send2trash(out_folder)
+    except:
+        shutil.rmtree(out_folder)
+
+
+def test_auto3D_config1():
+    """Check that the program runs"""
+    args = options(path, window=1, use_gpu=False, convergence_threshold=0.1,
+                   isomer_engine="omega", optimizing_engine="AIMNET")
+    out = main(args)
+    out_folder = os.path.dirname(os.path.abspath(out))
+    try:
+        send2trash(out_folder)
+    except:
+        shutil.rmtree(out_folder)
+
+
+def test_auto3D_config2():
+    """Check that the program runs"""
+    args = options(path, window=1, use_gpu=False, convergence_threshold=0.1,
+                   isomer_engine="rdkit", optimizing_engine="AIMNET", memory=2)
+    out = main(args)
+    out_folder = os.path.dirname(os.path.abspath(out))
+    try:
+        send2trash(out_folder)
+    except:
+        shutil.rmtree(out_folder)
+
+    
+def test_auto3D_config3():
+    """Check that the program runs"""
+    args = options(path, k=1, use_gpu=False, convergence_threshold=0.1,
+                   isomer_engine="rdkit", optimizing_engine="AIMNET", capacity=2)
+    out = main(args)
+    out_folder = os.path.dirname(os.path.abspath(out))
+    try:
+        send2trash(out_folder)
+    except:
+        shutil.rmtree(out_folder)
+
+
+def test_auto3D_config4():
+    """Check that the program runs"""
+    args = options(path, window=2, use_gpu=False, convergence_threshold=0.1,
+                   isomer_engine="omega", optimizing_engine="AIMNET", max_confs=1000)
+    out = main(args)
+    out_folder = os.path.dirname(os.path.abspath(out))
+    try:
+        send2trash(out_folder)
+    except:
+        shutil.rmtree(out_folder)
