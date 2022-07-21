e7370d1d2f7911835c9e585870550e656a9fd156
Zhen (Jack) Liu <liu5@andrew.cmu.edu>
2022-07-21 19:01:20 -0400

    change tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -23,13 +23,13 @@ def test_check_input():
         check_input(args2)
     assert(len(warnings_list2) == 0)
 
-    args3 = options(path2, k=1, use_gpu=False)  # by default enumerate_isomer=False
+    args3 = options(path2, k=1, use_gpu=False, enumerate_isomer=False)  # by default enumerate_isomer=True
     # count the number of warnings
     with warnings.catch_warnings(record=True) as warnings_list3:
         check_input(args3)
     assert(len(warnings_list3) >= 1)
 
-    args4 = options(path1, k=1, use_gpu=False)  # by default enumerate_isomer=False
+    args4 = options(path1, k=1, use_gpu=False, enumerate_isomer=False)  # by default enumerate_isomer=True
     # count the number of warnings
     with warnings.catch_warnings(record=True) as warnings_list4:
         check_input(args4)
