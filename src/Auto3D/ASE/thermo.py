4e08ac6aa777581ed7a156226158a3f887d2f5c6
LiuCMU <liu5@andrew.cmu.edu>
2023-11-10 18:00:04 -0500

    hessian vib for ani2x and ani2xt & refactor ani2xt forward function

--- src/Auto3D/ASE/thermo.py
+++ src/Auto3D/ASE/thermo.py
@@ -20,12 +20,9 @@ from ase.thermochemistry import IdealGasThermo
 import ase.calculators.calculator
 from functools import partial
 from typing import Optional
-try:
-    from ..batch_opt.ANI2xt_no_rep import ANI2xt
-except:
-    pass
 import torchani
 from Auto3D.batch_opt.batchopt import EnForce_ANI
+from Auto3D.batch_opt.ANI2xt_no_rep import ANI2xt
 from Auto3D.utils import hartree2ev
 
 torch.backends.cuda.matmul.allow_tf32 = False
@@ -83,30 +80,34 @@ def get_mol_idx_t3(mol):
     T = 298
     return (idx, T)
 
-def mol2aimnet_input(mol: Chem.Mol, device=torch.device('cpu')) -> dict:
+def mol2aimnet_input(mol: Chem.Mol, device=torch.device('cpu'), model_name='AIMNET') -> dict:
     """Converts sdf to aimnet input, assuming the sdf has only 1 conformer."""
     conf = mol.GetConformer()
     coord = torch.tensor(conf.GetPositions(), device=device).unsqueeze(0)
-    numbers = torch.tensor([atom.GetAtomicNum() for atom in mol.GetAtoms()], device=device).unsqueeze(0)
+    # if model_name == 'ANI2xt':
+    #     periodict2idx = {1:0, 6:1, 7:2, 8:3, 9:4, 16:5, 17:6}
+    #     numbers = torch.tensor([periodict2idx[a.GetAtomicNum()] for a in mol.GetAtoms()],
+    #                            device=device).unsqueeze(0)
+    # else:
+    numbers = torch.tensor([a.GetAtomicNum() for a in mol.GetAtoms()],
+                            device=device).unsqueeze(0)
     charge = torch.tensor([Chem.GetFormalCharge(mol)], device=device, dtype=torch.float)
     return dict(coord=coord, numbers=numbers, charge=charge)
 
 def model_name2model_calculator(model_name: str, device=torch.device('cpu'), charge=0):
     """Return a model and the ASE calculator object for a molecule"""
     if model_name == "ANI2xt":
-        model = EnForce_ANI(ANI2xt(device), model_name)
+        model = EnForce_ANI(ANI2xt(device, periodic_table_index=True), model_name).double()
         calculator = Calculator(model, charge)
     elif model_name == "AIMNET":
-        # Using a single AIMNet2 model for computing Hessian
-        # aimnet_0 = torch.jit.load(os.path.join(root, "models/aimnet2_wb97m-d3_0.jpt"), map_location=device)
-
         # Using the ensemble AIMNet2 model for computing energy and forces
         aimnet = torch.jit.load(os.path.join(root, "models/aimnet2_wb97m_ens_f.jpt"), map_location=device)
         model = EnForce_ANI(aimnet, model_name)
         calculator = Calculator(model, charge)
     elif model_name == "ANI2x":
-        model = torchani.models.ANI2x().to(device)
-        calculator = model.ase()
+        ani2x = torchani.models.ANI2x(periodic_table_index=True).to(device).double()
+        model = EnForce_ANI(ani2x, model_name)
+        calculator = ani2x.ase()
     else:
         raise ValueError("model has to be 'ANI2x', 'ANI2xt' or 'AIMNET'")
     return model, calculator
@@ -117,10 +118,11 @@ def mol2atoms(mol: Chem.Mol):
     species = [a.GetSymbol() for a in mol.GetAtoms()]
     atoms = Atoms(species, coord)
     return atoms
-    
-def vib_hessian(mol: Chem.Mol, ase_calculator, model, device=torch.device('cpu')):
+
+def vib_hessian(mol: Chem.Mol, ase_calculator, model,
+                device=torch.device('cpu'), model_name='AIMNET'):
     '''return a VibrationsData object
-    model: ANI2xt or AIMNet2 model with EnForce_ANI wrapper'''
+    model: ANI2xt or AIMNet2 or ANI2x that can be used to calculate Hessian'''
     # get the ASE atoms object
     coord = mol.GetConformer().GetPositions()
     species = [a.GetSymbol() for a in mol.GetAtoms()]
@@ -137,7 +139,8 @@ def vib_hessian(mol: Chem.Mol, ase_calculator, model, device=torch.device('cpu')
     hess_helper = partial(aimnet_hessian_helper,
                           numbers=numbers,
                           charge=charge,
-                          model=model)
+                          model=model,
+                          model_name=model_name)
     hess = torch.autograd.functional.hessian(hess_helper,
                                              coord)
     hess = hess.detach().cpu().view(num_atoms, 3, num_atoms, 3).numpy()    
@@ -163,9 +166,10 @@ def do_mol_thermo(mol: Chem.Mol,
                   atoms: ase.Atoms,
                   model: torch.nn.Module,
                   device=torch.device('cpu'),
-                  T=298.0):
-    """For a RDKit mol object, calculate its thermochemistry properties"""
-    vib = vib_hessian(mol, atoms.get_calculator(), model, device)
+                  T=298.0, model_name='AIMNET'):
+    """For a RDKit mol object, calculate its thermochemistry properties.
+    model: ANI2xt or AIMNet2 or ANI2x that can be used to calculate Hessian"""
+    vib = vib_hessian(mol, atoms.get_calculator(), model, device, model_name=model_name)
     vib_e = vib.get_energies()
     e = atoms.get_potential_energy()
     thermo = IdealGasThermo(vib_energies=vib_e,
@@ -183,7 +187,7 @@ def do_mol_thermo(mol: Chem.Mol,
     mol.SetProp("G_hartree", str(G))
     mol.SetProp("E_hartree", str(e * ev2hatree))
     
-    #Updating ASE atoms coordinates into pybel mol
+    #Updating ASE atoms coordinates into mol
     coord = atoms.get_positions()
     for i, atom in enumerate(mol.GetAtoms()):
         mol.GetConformer().SetAtomPosition(atom.GetIdx(), coord[i])
@@ -192,11 +196,25 @@ def do_mol_thermo(mol: Chem.Mol,
 def aimnet_hessian_helper(coord:torch.tensor, 
                           numbers:Optional[torch.Tensor]=None,
                           charge: Optional[torch.Tensor]=None,
-                          model: Optional[torch.nn.Module]=None):
-    dct = dict(coord=coord, numbers=numbers, charge=charge)
-    return model(dct)['energy']
+                          model: Optional[torch.nn.Module]=None,
+                          model_name='AIMNET'):
+    '''coord shape: (1, num_atoms, 3)
+    numbers shape: (1, num_atoms)
+    charge shape: (1,)'''
+    if model_name == 'AIMNET':
+        dct = dict(coord=coord, numbers=numbers, charge=charge)
+        return model(dct)['energy']  # energy unit: eV
+    elif model_name == 'ANI2xt':
+        device = coord.device
+        periodict2idx = {1:0, 6:1, 7:2, 8:3, 9:4, 16:5, 17:6}
+        numbers2 = torch.tensor([periodict2idx[num.item()] for num in numbers.squeeze()], device=device).unsqueeze(0)
+        e = model(numbers2, coord)
+        return e  # energy unit: eV
+    elif model_name == 'ANI2x':
+        e = model((numbers, coord)).energies * hartree2ev
+        return e  # energy unit: eV
 
-def calc_thermo(path: str, model_name: str, get_mol_idx_t=None, gpu_idx=0, opt_tol=0.0005, opt_steps=5000):
+def calc_thermo(path: str, model_name: str, get_mol_idx_t=None, gpu_idx=0, opt_tol=0.0002, opt_steps=5000):
     """ASE interface for calculation thermo properties using ANI2x, ANI2xt or AIMNET
     path: Input sdf file
     model_name: ANI2x, ANI2xt or AIMNET
@@ -205,29 +223,29 @@ def calc_thermo(path: str, model_name: str, get_mol_idx_t=None, gpu_idx=0, opt_t
     opt_tol: Convergence_threshold for geometry optimization
     opt_steps: Maximum geometry optimizaiton steps"""
     #Prepare output name
+    out_mols, mols_failed = [], []
     dir = os.path.dirname(path)
     basename = os.path.basename(path).split(".")[0] + f"_{model_name}_G.sdf"
     outpath = os.path.join(dir, basename)
 
-    #internal parameters
-    out_mols = []
-    mols_failed = []
-    species2numbers = {'H':1, 'C':6, 'N':7, 'O':8, 'F':9, 'Si':14, 'P':15,
-                       'S':16, 'Cl':17, 'As':33, 'Se':34, 'Br':35, 'I':53, 'B':5}
-    numbers2species = dict([(val, key) for (key, val) in species2numbers.items()])
     if torch.cuda.is_available():
         device = torch.device(f"cuda:{gpu_idx}")
     else:
         device = torch.device("cpu")
 
     if model_name == 'AIMNET':
-        aimnet_0 = torch.jit.load(os.path.join(root, "models/aimnet2_wb97m-d3_0.jpt"), map_location=device)
+        aimnet0_path = os.path.join(root, "models/aimnet2_wb97m-d3_0.jpt")
+        hessian_model = torch.jit.load(aimnet0_path, map_location=device)
+    elif model_name == 'ANI2xt':
+        hessian_model = ANI2xt(device).double()
+    elif model_name == 'ANI2x':
+        hessian_model = torchani.models.ANI2x(periodic_table_index=True).to(device).double()
     model, calculator = model_name2model_calculator(model_name, device)
 
     mols = list(Chem.SDMolSupplier(path, removeHs=False))
     for mol in tqdm(mols):
         coord = mol.GetConformer().GetPositions()
-        species = [numbers2species[a.GetAtomicNum()] for a in mol.GetAtoms()]
+        species = [a.GetSymbol() for a in mol.GetAtoms()]
         charge = rdmolops.GetFormalCharge(mol)
         atoms = Atoms(species, coord)
 
@@ -241,27 +259,29 @@ def calc_thermo(path: str, model_name: str, get_mol_idx_t=None, gpu_idx=0, opt_t
         else:
             idx, T = get_mol_idx_t(mol)
         print(idx)
+
         try:
             try:
                 try:
-                    # THE FOLLOWING CODE ONLY WORKS FOR AIMNET2
-                    aimnet_in = mol2aimnet_input(mol, device)
-                    _, f_ = model(aimnet_in['coord'], aimnet_in['numbers'], aimnet_in['charge'])
+                    EnForce_in = mol2aimnet_input(mol, device, model_name=model_name)
+                    _, f_ = model(EnForce_in['coord'].requires_grad_(True),
+                                    EnForce_in['numbers'],
+                                    EnForce_in['charge'])
                     fmax = f_.norm(dim=-1).max(dim=-1)[0].item()
                     assert fmax <= 0.01
-                    mol = do_mol_thermo(mol, atoms, aimnet_0, device, T)
+                    mol = do_mol_thermo(mol, atoms, hessian_model, device, T, model_name=model_name)
                     out_mols.append(mol)
                 except AssertionError:
                     print('optiimize the input geometry')
                     opt = BFGS(atoms)
                     opt.run(fmax=3e-3, steps=opt_steps)
-                    mol = do_mol_thermo(mol, atoms, aimnet_0, device, T)
+                    mol = do_mol_thermo(mol, atoms, hessian_model, device, T, model_name=model_name)
                     out_mols.append(mol)
             except ValueError:
                 print('use tighter convergence threshold for geometry optimization')
                 opt = BFGS(atoms)
                 opt.run(fmax=opt_tol, steps=opt_steps)
-                mol = do_mol_thermo(mol, atoms, aimnet_0, device, T)
+                mol = do_mol_thermo(mol, atoms, hessian_model, device, T, model_name=model_name)
                 out_mols.append(mol)
         except:
             print("Failed: ", idx, flush=True)
@@ -276,5 +296,8 @@ def calc_thermo(path: str, model_name: str, get_mol_idx_t=None, gpu_idx=0, opt_t
     return outpath
 
 if __name__ == "__main__":
-    path = '/home/jack/run_auto3d/20231030-101405-214461_methane/imaginary/methane_out.sdf'
-    out = calc_thermo(path, 'AIMNET', get_mol_idx_t3, gpu_idx=2)
+    # path = '/home/jack/run_auto3d/20231030-101405-214461_methane/imaginary/methane_out.sdf'
+    path = '/home/jack/Auto3D_pkg/tests/files/cyclooctane.sdf'
+    # out = calc_thermo(path, 'AIMNET', gpu_idx=1)
+    out = calc_thermo(path, 'ANI2xt', gpu_idx=1)
+    # out = calc_thermo(path, 'ANI2x', gpu_idx=1)
